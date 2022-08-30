14cbe9c0221928b50217c06b885a31eb20260746
RichardPMachado <richardmachado2211@gmail.com>
2022-08-30 17:49:52 -0300

    Requisito 2 - completo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'; // https://v5.reactrouter.com/web/api/Redirect
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  state = {
+    loginNameInput: '',
+    isDisabled: true,
+    loading: false,
+    redirect: false,
+    // data: [],
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { loginNameInput } = this.state;
+      const numMin = 3;
+
+      if (loginNameInput.length >= numMin) {
+        this.setState({ isDisabled: false });
+      } else {
+        this.setState({ isDisabled: true });
+      }
+    });
+  };
+
+  onButtonClick = async () => {
+    this.setState({ loading: true });
+    const { loginNameInput } = this.state;
+    await createUser({ name: loginNameInput });
+    this.setState({ redirect: true });
+  };
+
   render() {
+    const { loginNameInput, isDisabled, loading, redirect } = this.state;
+    const LoginScreen = (
+
+      <form>
+        <label htmlFor="loginNameInput">
+          <p>Nome</p>
+          <input
+            value={ loginNameInput }
+            data-testid="login-name-input"
+            type="text"
+            name="loginNameInput"
+            onChange={ this.onInputChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ isDisabled }
+          onClick={ this.onButtonClick }
+        >
+          Entrar
+        </button>
+      </form>
+
+    );
     return (
-      <div data-testid="page-login">Login</div>
+      redirect ? <Redirect to="/search" />
+        : (
+          <div data-testid="page-login">
+            { loading ? <Loading />
+              : LoginScreen }
+          </div>
+        )
     );
   }
 }
