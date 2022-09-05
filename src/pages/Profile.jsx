774799242e341f45d57cbef08256515a0714d5d4
RichardPMachado <richardmachado2211@gmail.com>
2022-09-05 15:58:05 -0300

    requisito 13 - completo

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { Switch, Route, Link } from 'react-router-dom';
 import Header from '../componentes/Header';
-import ProfileEdit from './ProfileEdit';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Profile extends Component {
+  state = {
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+    isLoading: false,
+    isR: 'rferte',
+  };
+
+  componentDidMount() {
+    this.getProfileData();
+  }
+
+  getProfileData = async () => {
+    this.setState({ isLoading: true });
+    const user = await getUser();
+    this.setState({
+      name: user.name,
+      email: user.email,
+      image: user.image,
+      description: user.description,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { name, email, image, description, isLoading, isR } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-profile">
-          <Switch>
-            <Route
-              path="/profile/edit"
-              component={ ProfileEdit }
-            />
-          </Switch>
-        </div>
+        { isLoading ? <Loading /> : (
+          <div data-testid="page-profile">
+            <img src={ image } alt={ name } data-testid="profile-image" />
+            <Link to="/profile/edit" Loading={ isR }>Editar perfil</Link>
+            <h2>Nome</h2>
+            <h3>{ name }</h3>
+            <h2>E-mail</h2>
+            <h3>{ email }</h3>
+            <h2>Descrição</h2>
+            <h3>{ description }</h3>
+          </div>
+        )}
       </>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ProfileEdit extends Component {
   render() {
+    const { name, email, image, description, isR } = this.props;
     return (
-      <div data-testid="page-profile-edit">ProfileEdit</div>
+      <div data-testid="page-profile-edit">
+        <h1>{ name }</h1>
+        <h2>{ email }</h2>
+        <img src={ image } alt={ name } />
+        <p>{ description }</p>
+        <p>{isR}</p>
+      </div>
     );
   }
 }
+
+ProfileEdit.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  isR: PropTypes.string.isRequired,
+};
